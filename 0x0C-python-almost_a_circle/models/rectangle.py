c399efe6feda28bca56fe7194a6e811cdb3c0b68
Olah Daniel <danielolahskybrow77@gmail.com>
2023-01-15 22:03:52 +0100

    updates to object initialization

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,10 +4,10 @@ from models.base import Base
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         super(Rectangle, self).__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
