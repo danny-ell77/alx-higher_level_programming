b511f801ee1eef9ad0082eb5345e9f0a712cbced
Daniel Olah <danielolahskybrow77@gmail.com>
2023-03-19 23:58:55 +0000

    reviewed task &

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,12 +11,10 @@ script that lists all State objects from the database hbtn_0e_6_usa
 
 if __name__ == '__main__':
     import sys
-    import warnings
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
     from model_state import Base, State
 	
-    warnings.filterwarnings('ignore', message=".*'@@SESSION.GTID_EXECUTED' is deprecated.*")
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
