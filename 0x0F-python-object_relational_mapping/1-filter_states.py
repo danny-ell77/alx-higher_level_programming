882e4beb331b03bbd5fcf82c7095bec1f3642530
Olah Daniel <danielolahskybrow77@gmail.com>
2023-03-20 01:14:40 +0100

    resolved tasks 1 & 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     cursor.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+        "SELECT * FROM states WHERE name LIKE BINARY'N%' ORDER BY id ASC;")
 
     results = cursor.fetchall()
 
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -25,7 +25,8 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     cursor.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(value)
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC;".format(
+            value)
     )
 
     results = cursor.fetchall()
