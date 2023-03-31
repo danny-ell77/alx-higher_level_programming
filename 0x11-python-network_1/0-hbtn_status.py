5e01f7bb7881a1cc5714c400b4b057a0ba46270a
Daniel Olah <danielolahskybrow77@gmail.com>
2023-03-31 13:20:06 +0100

    made all modules scripts

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,8 @@
 
 from urllib import request
 
-with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    content_bytes = response.read()
-    content_utf8 = content_bytes.decode('utf-8')
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(type(content_bytes), content_bytes, content_utf8))
+if __name__ == "__main__":
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        content_bytes = response.read()
+        content_utf8 = content_bytes.decode('utf-8')
+        print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(type(content_bytes), content_bytes, content_utf8))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,7 +5,8 @@ the value of the X-Request-Id variable found in the header of the response."""
 import sys
 from urllib import request
 
-url = sys.argv[1]
-with request.urlopen(url) as response:
-    print(response.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with request.urlopen(url) as response:
+        print(response.headers.get('X-Request-Id'))
     
