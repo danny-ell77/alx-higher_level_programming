7f3b804cefe3841544e922653c5fc30aa3db64af
Olah Daniel <danielolahskybrow77@gmail.com>
2023-04-26 04:57:10 +0000

    finished advanced tasks

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,15 +2,14 @@
 const fs = require('fs');
 const request = require('request');
 
-
 const filename = process.argv[3];
 
 let content;
 
 request(process.argv[2], function (error, response, body) {
-	if (error) throw error;
-	console.log(body)
-	fs.writeFile(filename, body, (err) => {
+  if (error) throw error;
+  console.log(body);
+  fs.writeFile(filename, body, (err) => {
 	  if (err) console.log(err);
-	});
+  });
 });
